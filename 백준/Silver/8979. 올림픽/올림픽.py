d5d255a75a7a7b974b6a4a8483e7472e3afed741
yunnn426 <95147113+yunnn426@users.noreply.github.com>
2023-09-10 12:16:40 +0900

    [Silver V] Title: 올림픽, Time: 80 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -10,26 +10,12 @@ for i in range(N):
     total = 3 * medal[1] + 2 * medal[2] + 1 * medal[3] #나라 점수
     score[medal[0] - 1] = total
 
-rank_tmp = 1 #등수
-count = 0 #몇번째 score인지 추적용
-while(max(score) != 0):
-    max_arr = [] #최대 점수들의 인덱스 저장
-    max_score = max(score)
 
-    #현재 score 배열에서 가장 큰 점수를 가진 나라의 인덱스를 저장
-    for i in range(len(score)):
-        if max_score == score[i]:
-            max_arr.append(i)
-    
-    #그 나라들에게 등수 부여
-    for i in range(len(max_arr)):
-        rank[max_arr[i]] = rank_tmp
+k_score = score[K - 1] #알고자 하는 나라의 점수
+count = 1 #등수
+#K 나라보다 점수가 높은 나라만큼 등수에 더함
+for i in range(N):
+    if score[i] > k_score: 
+        count += 1
 
-    #등수 계산 완료한 나라는 score 배열에서 제거
-    for i in range(len(max_arr)):
-        score[max_arr[i]] = 0
-        rank_tmp += 1
-    
-    count += 1
-
-print(rank[K - 1])
+print(count)
