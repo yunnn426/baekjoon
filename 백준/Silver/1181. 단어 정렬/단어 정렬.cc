87f23cf4f71fe1fe2ceefebb16390cf546b01c31
yunnn426 <95147113+yunnn426@users.noreply.github.com>
2024-02-01 01:10:50 +0900

    [Silver V] Title: 단어 정렬, Time: 52 ms, Memory: 7468 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.cc
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -1,27 +1,26 @@
 #include <iostream>
 #include <string>
 #include <set>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-struct myOrder {
-    bool operator() (const string &a, const string &b) const {
-        if (a.size() == b.size()) {
-            return a < b;
-        }
-        else {
-            return a.size() < b.size();
-        }
-    }
-};
+bool longer(string a, string b) {
+    if (a.size() == b.size())
+        return a < b;
+    else
+        return a.size() < b.size();
+}
 
 int main(void) {
     cin.tie(NULL); ios_base::sync_with_stdio(false);
     
     int n;
     cin >> n;
-    set<string, myOrder> inputs;
-
+    set<string> inputs;
+    vector<string> answer;
+    
     for (int tc = 0; tc < n; tc++) {
         string tmp;
         cin >> tmp;
@@ -29,9 +28,14 @@ int main(void) {
     }
     
     for (auto iter = inputs.begin(); iter != inputs.end(); iter++) {
-        cout << *iter << endl;
+        answer.push_back(*iter);
     }
-
+    
+    sort(answer.begin(), answer.end(), longer);
+    
+    for (auto iter = answer.begin(); iter != answer.end(); iter++) {
+        cout << *iter << "\n";
+    }
     
     return 0;
 }
