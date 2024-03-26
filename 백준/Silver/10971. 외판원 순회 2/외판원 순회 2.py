cf688a2513ea9cb620c791810028e63e98ea5520
Yunkyung Hwang <yunnn99@gmail.com>
2024-03-26 21:24:15 +0900

    [Silver II] Title: 외판원 순회 2, Time: 1244 ms, Memory: 113068 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -24,15 +24,14 @@ def search(start, to, cost, cnt):
         search(start, i, cost + costarr[to][i], cnt + 1)
         visited[i] = 0
 
-if __name__ == '__main__':
-    for _ in range(n):
-        arr = list(map(int, sys.stdin.readline().split()))
-        costarr.append(arr)
+for _ in range(n):
+    arr = list(map(int, sys.stdin.readline().split()))
+    costarr.append(arr)
 
-    # 1번 도시부터 완전탐색
-    for i in range(n):
-        visited[i] = 1
-        search(i, i, 0, 1)
-        visited[i] = 0
+# 1번 도시부터 완전탐색
+for i in range(n):
+    visited[i] = 1
+    search(i, i, 0, 1)
+    visited[i] = 0
 
-    print(answer)
+print(answer)
