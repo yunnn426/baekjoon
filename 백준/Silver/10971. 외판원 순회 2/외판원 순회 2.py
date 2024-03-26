ee2bbd03bc90063c17283210920e7f224d10c499
Yunkyung Hwang <yunnn99@gmail.com>
2024-03-26 21:28:16 +0900

    [Silver II] Title: 외판원 순회 2, Time: 172 ms, Memory: 111736 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -5,12 +5,16 @@ visited = [0 for _ in range(n)]
 answer = sys.maxsize
 
 def search(start, to, cost, cnt):
+    global answer
+    
+    if cost > answer: # 비용이 넘어선 경우 더 확인하지 않음
+        return
+    
     if cnt == n:
         if costarr[to][start] == 0: # 출발지로 돌아가는 길이 없음
             return
         else:
             cost += costarr[to][start] # 출발지로 돌아감
-        global answer
         answer = min(cost, answer)
         return
 
@@ -24,14 +28,15 @@ def search(start, to, cost, cnt):
         search(start, i, cost + costarr[to][i], cnt + 1)
         visited[i] = 0
 
-for _ in range(n):
-    arr = list(map(int, sys.stdin.readline().split()))
-    costarr.append(arr)
+if __name__ == '__main__':
+    for _ in range(n):
+        arr = list(map(int, sys.stdin.readline().split()))
+        costarr.append(arr)
 
-# 1번 도시부터 완전탐색
-for i in range(n):
-    visited[i] = 1
-    search(i, i, 0, 1)
-    visited[i] = 0
+    # 1번 도시부터 완전탐색
+    for i in range(n):
+        visited[i] = 1
+        search(i, i, 0, 1)
+        visited[i] = 0
 
-print(answer)
+    print(answer)
