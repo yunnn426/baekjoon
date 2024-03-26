826832469207f47f9d5e5bdda633e3282d09400f
Yunkyung Hwang <yunnn99@gmail.com>
2024-03-26 22:47:24 +0900

    [Silver II] Title: 나무 자르기, Time: 500 ms, Memory: 258028 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -0,0 +1,26 @@
+import sys
+n, m = map(int, sys.stdin.readline().split())
+tree = list(map(int, sys.stdin.readline().split()))
+
+start = 0
+end = 1000000000
+answer = 0
+
+while start <= end:
+    mid = (start + end) // 2
+
+    sum = 0
+    for t in tree:
+        cut = t - mid
+        if cut >= 0:
+            sum += cut
+
+    if sum >= m:
+        answer = mid
+        start = mid + 1
+        
+    else:
+        end = mid - 1
+    
+
+print(answer)
