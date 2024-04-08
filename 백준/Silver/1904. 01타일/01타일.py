836af5bf7252062e7112c8bcc16284846f195f25
Yunkyung Hwang <yunnn99@gmail.com>
2024-04-08 21:12:38 +0900

    [Silver III] Title: 01타일, Time: 120 ms, Memory: 117056 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.py
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -1,12 +1,23 @@
 import sys
 input = sys.stdin.readline
 
+# 1개
+# 1
+
+# 2개
+# 00, 11
+ 
+# 3개
+# 001, 100, 111
+
+# 4개
+# 0000, 1100, 0011, 1001, 1111
+
 n = int(input())
 dp = [0 for _ in range(n + 10)]
-dp[0] = 0
 dp[1] = 1
 dp[2] = 2
 for i in range(3, n + 1):
     dp[i] = (dp[i - 1] + dp[i - 2]) % 15746
 
-print(dp[n] % 15746)
+print(dp[n])
