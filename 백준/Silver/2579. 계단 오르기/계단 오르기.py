1dbe21bf488839d89ab7c54a350c02f198c7a6de
Yunkyung Hwang <yunnn99@gmail.com>
2024-04-11 10:10:24 +0900

    [Silver III] Title: 계단 오르기, Time: 104 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,28 +1,26 @@
+# 계단 오르기
 import sys
 input = sys.stdin.readline
 
 n = int(input())
-stair = []
-stair.append(0)
+stair = [0]
 for _ in range(n):
     s = int(input())
     stair.append(s)
 
 dp = [0 for _ in range(n + 1)]
-
 for i in range(1, n + 1):
     if i == 1:
-        dp[1] = stair[1]
+        dp[i] = stair[1]
     elif i == 2:
-        dp[2] = dp[1] + stair[2]
+        dp[i] = stair[1] + stair[2]
     elif i == 3:
-        dp[3] = max(stair[1] + stair[3], stair[2] + stair[3])
+        dp[i] = max(stair[1] + stair[3], stair[2] + stair[3])
+    
+    # 직전 계단을 밟거나 그 전 계단을 밟는 경우를 고려함
     else:
-        # i번째 계단의 최대 점수
-        # = 
-        # i-2번째 계단까지의 최대 점수 + i번째 계단의 점수
-        # i번째 계단의 점수 + i-1번째 계단의 점수 + i-3번째 계단까지의 점수 (연달아 세개 못올라가니까)
-        # 중 큰 값
+        # i-2까지 오른 후 i에 오르는 경우
+        # i-3까지 오른 후 i-1에 오르고 i에 오르는 경우
         dp[i] = max(dp[i - 2] + stair[i], dp[i - 3] + stair[i - 1] + stair[i])
 
 print(dp[n])
