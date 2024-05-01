acb849d0d3f0bea8a775b721efb03bfe3d736c5a
Yunkyung Hwang <yunnn99@gmail.com>
2024-05-01 10:24:12 +0900

    [level 2] Title: 전화번호 목록, Time: 168.06 ms, Memory: 48.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
 
 ### 성능 요약
 
-메모리: 31.6 MB, 시간: 74.07 ms
+메모리: 48.7 MB, 시간: 168.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 04일 23:41:59
+2024년 05월 01일 10:24:09
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -1,17 +1,26 @@
 #include <string>
 #include <vector>
+#include <map>
 #include <algorithm>
-#include <iostream>
 
 using namespace std;
 
 bool solution(vector<string> phone_book) {
     bool answer = true;
+    map<string, int> m;
     sort(phone_book.begin(), phone_book.end());
     
-    for (int i = 0; i < phone_book.size() - 1; i++) {
-        if (phone_book[i] == phone_book[i + 1].substr(0, phone_book[i].size()))
+    int cnt = 0;
+    for (auto p : phone_book) {
+        string sub = p.substr(0, cnt);
+        if (m[sub] == 0) { // 접두사가 아니면 번호 맵에 추가
+            m[p] = 1;
+        }
+        else {
             return false;
+        }
+        cnt = p.length();
     }
+        
     return answer;
 }
