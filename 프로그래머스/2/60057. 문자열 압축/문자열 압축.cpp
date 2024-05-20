a60fceb97ff7fbf454622a7b47728e20d98b5f22
Yunkyung Hwang <yunnn99@gmail.com>
2024-05-20 23:25:34 +0900

    [level 2] Title: 문자열 압축, Time: 0.02 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.cpp
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.cpp
@@ -10,7 +10,6 @@ int solution(string s) {
     
     for (int cut = 1; cut <= n / 2; cut++) {
         int answer = 0;
-        
         /* cut만큼 잘라서 배열에 추가 */
         vector<string> subarr;
         int st = 0, ed = n - 1;
@@ -22,23 +21,22 @@ int solution(string s) {
         }
         subarr.push_back("end");
         
-        string cmp = subarr[0];
-        int cnt = 1;
-        for (int i = 1; i < subarr.size(); i++) {
-            // 비교하는 문자열이랑 같으면 카운트 +1
+        string cmp = "";
+        int cnt = 0;
+        for (int i = 0; i < subarr.size(); i++) {
+            if (cmp == "" || cmp != subarr[i]) {
+                // 이전 문자열 처리
+                if (cnt > 1)
+                    answer += 1;
+                cnt = 0;
+                
+                cmp = subarr[i];
+                answer += cut;
+                continue;
+            }
+            
             if (cmp == subarr[i]) {
                 cnt += 1;
-            }
-            // 다르면
-            else {
-                // 문자열 길이만큼 더하고
-                answer += cmp.length();
-                // 2 이상인 경우 +길이
-                if (cnt > 1)
-                    answer += to_string(cnt).length();
-                // 다시 초기화
-                cnt = 1;
-                cmp = subarr[i];
             }
         }
         
