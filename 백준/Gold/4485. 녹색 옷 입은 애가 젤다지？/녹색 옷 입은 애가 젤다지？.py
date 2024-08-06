47d4823492b92e6dda5d8a9509fc4f57a565e315
Yunkyung Hwang <yunnn99@gmail.com>
2024-08-06 23:07:48 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 308 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
@@ -2,23 +2,15 @@ from collections import deque
 import sys
 input = sys.stdin.readline
 
-def bfs(n, cave):
+def bfs(n, cave, visited):
     dx = [1, -1, 0, 0]
     dy = [0, 0, -1, 1]
 
     q = deque()
-    q.append((0, 0, cave[0][0])) # x, y, cost
-    visited = [[1e9 for _ in range(n)] for _ in range(n)]
-    visited[0][0] = cave[0][0]
-    answer = 1e9
+    q.append((0, 0)) # x, y
 
     while q:
-        x, y, cost = q.popleft()
-
-        # 출구 도착
-        if x == n - 1 and y == n - 1:   
-            answer = min(answer, cost)
-            continue
+        x, y = q.popleft()
 
         for i in range(4):
             nx = x + dx[i]
@@ -27,13 +19,12 @@ def bfs(n, cave):
             if not 0 <= nx < n or not 0 <= ny < n:
                 continue
             # 이미 해당 칸에 도착할 수 있는 더 짧은 경로가 있으면 패스
-            if visited[nx][ny] <= cost + cave[nx][ny]:
+            # = 안하면 메모리 초과
+            if visited[nx][ny] <= visited[x][y] + cave[nx][ny]:
                 continue
 
-            q.append((nx, ny, cost + cave[nx][ny]))
-            visited[nx][ny] = cost + cave[nx][ny]
-
-    return answer
+            q.append((nx, ny))
+            visited[nx][ny] = visited[x][y] + cave[nx][ny]
 
 idx = 0
 while True:
@@ -42,7 +33,9 @@ while True:
     if n == 0: break
 
     cave = [list(map(int, input().split())) for _ in range(n)]
+    visited = [[1e9 for _ in range(n)] for _ in range(n)]
+    visited[0][0] = cave[0][0]
 
-    answer = bfs(n, cave)
+    answer = bfs(n, cave, visited)
 
-    print('Problem ', idx, ': ', answer, sep='')
+    print('Problem ', idx, ': ', visited[n - 1][n - 1], sep='')
