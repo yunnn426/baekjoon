432fd5db3fcdd8073a1eaf9dffed5700e30923a8
Yunkyung Hwang <yunnn99@gmail.com>
2024-08-24 23:13:37 +0900

    [Gold V] Title: 호석이 두 마리 치킨, Time: 148 ms, Memory: 111596 KB, Score: 19 point -BaekjoonHub

--- 백준/Gold/21278. 호석이 두 마리 치킨/호석이 두 마리 치킨.py
+++ 백준/Gold/21278. 호석이 두 마리 치킨/호석이 두 마리 치킨.py
@@ -1,40 +1,26 @@
-from collections import deque
-import sys
-input = sys.stdin.readline
-
 n, m = map(int, input().split())
-dist = [[1e9 for _ in range(n + 1)] for _ in range(n + 1)]
+INF = int(1e9)
+dist = [[INF] * (n + 1) for _ in range(n + 1)]
 
 for _ in range(m):
     a, b = map(int, input().split())
-    dist[a][b] = 1
-    dist[b][a] = 1
+    dist[a][b] = dist[b][a] = 1
 
-# 모든 건물 사이 거리 구하기
 for i in range(1, n + 1):
-    for j in range(1, n + 1):
-        if i == j:
-            dist[i][j] = 0
-            continue
-        
-        for k in range(1, n + 1):
+    dist[i][i] = 0
+
+for k in range(1, n + 1):
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
             dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
 
-answer = 1e9
-a = 0
-b = 0
-# i, j에 치킨집
-for i in range(1, n + 1):
-    for j in range(1, n + 1):
-        sum = 0
+answer = INF
+a, b = 0, 0
 
-        for k in range(1, n + 1):
-            sum += min(dist[i][k], dist[j][k])
-        
-        if sum < answer:
-            a = i
-            b = j
-            answer = sum
-    
+for i in range(1, n):
+    for j in range(i + 1, n + 1):
+        total = sum(min(dist[i][k], dist[j][k]) for k in range(1, n + 1))
+        if total < answer:
+            a, b, answer = i, j, total
+
 print(a, b, 2 * answer)
-            
