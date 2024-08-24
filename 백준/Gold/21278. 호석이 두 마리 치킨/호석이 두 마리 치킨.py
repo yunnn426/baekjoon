101114399fd70f0fcce096817d349f17ba392ef6
Yunkyung Hwang <yunnn99@gmail.com>
2024-08-24 22:56:02 +0900

    [Gold V] Title: 호석이 두 마리 치킨, Time: 172 ms, Memory: 114772 KB, Score: 12 point -BaekjoonHub

--- 백준/Gold/21278. 호석이 두 마리 치킨/호석이 두 마리 치킨.py
+++ 백준/Gold/21278. 호석이 두 마리 치킨/호석이 두 마리 치킨.py
@@ -3,50 +3,38 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-road = [[] for _ in range(n + 1)]
+dist = [[1e9 for _ in range(n + 1)] for _ in range(n + 1)]
 
 for _ in range(m):
     a, b = map(int, input().split())
-    road[a].append(b)    
-    road[b].append(a)
+    dist[a][b] = 1
+    dist[b][a] = 1
 
-def bfs(k, i, j):
-    visited = [0 for _ in range(n + 1)]
-    q = deque()
-    q.append((k, 0))
-    visited[k] = 1
-
-    while q:
-        x, time = q.popleft()
-
-        if x == i or x == j:
-          return time  
-
-        for i in road[x]:
-            if visited[i] != 0:
-                continue
-
-            q.append((i, time + 1))
-            visited[i] = 1
+# 모든 건물 사이 거리 구하기
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        if i == j:
+            dist[i][j] = 0
+            continue
+        
+        for k in range(1, n + 1):
+            dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
 
 answer = 1e9
 a = 0
 b = 0
+# i, j에 치킨집
 for i in range(1, n + 1):
-    for j in range(i + 1, n + 1):
-        # 건물 i, j에 치킨집
+    for j in range(1, n + 1):
         sum = 0
 
-        # 건물 k로부터 i, j까지의 거리 구하기
         for k in range(1, n + 1):
-            if k == i or k == j:
-                continue
-                
-            sum += 2 * bfs(k, i, j)
+            sum += min(dist[i][k], dist[j][k])
         
         if sum < answer:
             a = i
             b = j
             answer = sum
-
-print(a, b, answer)
+    
+print(a, b, 2 * answer)
+            
