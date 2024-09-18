5c2298bd777eab8a04136841d38996abc33de14e
Yunkyung Hwang <yunnn99@gmail.com>
2024-09-18 23:33:29 +0900

    [Silver I] Title: RGB거리, Time: 104 ms, Memory: 110948 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -2,27 +2,22 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-cost = []
-for i in range(n):
-    r, g, b = list(map(int, input().split()))
-    cost.append((r, g, b))
+cost = [list(map(int, input().split())) for _ in range(n)]
 
-dp = [[0 for _ in range(n + 1)] for _ in range(3)]
-for h in range(1, n + 1): # 1~n번 집
-    for c in range(3): # R, G, B
-        if h == 1:
-            dp[c][h] = cost[0][c]
-            continue
+dp = [[1e9 for _ in range(3)] for _ in range(n)]
+for i in range(3):
+    dp[0][i] = cost[0][i]
 
-        if c == 0: # R
-            # 이전 집을 B or G로 칠했을 때 최소값 가져옴
-            dp[c][h] = min(dp[1][h - 1], dp[2][h - 1]) + cost[h - 1][c]
+for i in range(1, n):
+    for j in range(3):
+        # 직전 집까지 칠하는 가장 저렴한 비용 구하기
+        before = 1e9
+        for k in range(3):
+            if k == j: 
+                continue
+            before = min(before, dp[i - 1][k])
         
-        elif c == 1: # G
-            dp[c][h] = min(dp[0][h - 1], dp[2][h - 1]) + cost[h - 1][c]
+        # 위에서 구한 비용 + j번째 색으로 칠하는 비용
+        dp[i][j] = cost[i][j] + before
 
-        else:
-            dp[c][h] = min(dp[0][h - 1], dp[1][h - 1]) + cost[h - 1][c]
-
-# n번째 집을 칠하는 최소값
-print(min(dp[0][n], dp[1][n], dp[2][n]))
+print(min(dp[n - 1]))
